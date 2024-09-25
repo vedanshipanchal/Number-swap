8d93b58cc2ca364454a330e17d4eb23085cec518
vedanshipanchal <vedanshipanchal@gmail.com>
2024-09-25 21:57:38 +0100

    Number swap

---
+++ main.py
@@ -0,0 +1,8 @@
+a = input("Enter your name: ")
+b = input("Enter your name: ")
+c = input("Enter your name: ")
+change = a
+a = b
+b = c
+c = change
+print(f"The correct position is: a = {a}, b = {b}, c = {c}")
